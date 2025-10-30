1493537b72ec5334bd4ab4db3c63ddc725f5c9c4
Lukman Memon <memon_lukman@hotmail.com>
2025-10-30 19:17:51 -0400

    Updated backend files for production

--- backend/app/utils/qdrant_client.py
+++ backend/app/utils/qdrant_client.py
@@ -1,8 +1,5 @@
 from qdrant_client import QdrantClient
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 QDRANT_URL = os.getenv("QDRANT_URL")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
