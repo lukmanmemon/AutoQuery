9ee1e6c97381ad515147dcf0ae123eff2c2427ed
Lukman Memon <memon_lukman@hotmail.com>
2025-10-31 21:43:47 -0400

    Reduce memory usage by freeing resources and using smaller transformer model

--- backend/app/services/embeddings.py
+++ backend/app/services/embeddings.py
@@ -1,17 +1,19 @@
 from sentence_transformers import SentenceTransformer
-from functools import lru_cache
+import gc
 
-@lru_cache(maxsize=1)
 def get_model():
     """
     Lazy-load the SentenceTransformer model the first time it's needed.
     Keeps it cached afterward.
     """
-    return SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    return SentenceTransformer("sentence-transformers/paraphrase-MiniLM-L3-v2")
 
 def get_embedding(text: str) -> list[float]:
     model = get_model()  # Load only when first used
     text = text.strip().replace("\n", " ")
     embedding = model.encode(text, convert_to_numpy=True).tolist()
+    del model # Free memory immediately
+    gc.collect()
+    
     return embedding
 
